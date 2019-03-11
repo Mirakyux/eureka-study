cfd410bfcdeb43f061ad54c604dd685ceab169c0
nttd-zhangheng <38005710+nttd-zhangheng@users.noreply.github.com>
2019-03-11 17:30:24 +0800

    #new

---
+++ src/main/java/com/mirakyux/eurekastudy/EurekaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mirakyux.eurekastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mirakyux/eurekastudy/EurekaStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mirakyux.eurekastudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
